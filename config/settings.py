8947dc45609a6893c8af1bb779a4d143ec066fd9
alexis <mailalesci@gmail.com>
2025-03-01 14:19:53 +0100

    FEAT: improve navbar design

--- config/settings.py
+++ config/settings.py
@@ -148,5 +148,7 @@ MARKDOWNIFY = {
 }
 
 # HTMX
-CSRF_TRUSTED_ORIGINS = ['https://*.kenshuri.com','https://*.127.0.0.1', 'https://*.blog-production-0709.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://*.kenshuri.com','https://*.127.0.0.1',
+                        'https://*.blog-production-0709.up.railway.app',
+                        'https://blog-dev-205e.up.railway.app']
 
