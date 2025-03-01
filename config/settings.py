22c76183c1492e13879609464c260771a1e0657b
alexis <mailalesci@gmail.com>
2025-03-01 14:04:13 +0100

    BUILD: test deploy dev

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['blog-production-0709.up.railway.app',
                  'blog.kenshuri.com',
-                 '127.0.0.1']
+                 '127.0.0.1',
+                 'blog-dev-205e.up.railway.app']
 
 
 # Application definition
