d8eeaba3aede3edf0c0c3f5a3d8b02c7b82e0d8e
alexis <mailalesci@gmail.com>
2025-03-03 14:41:49 +0100

    FEAT: Add ssecret key

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2%rydirhvzb1#%il_iq7azm2l0sb0l3fdf@+n20=#%d@+m5bm$'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG") == 'True'
